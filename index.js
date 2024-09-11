aef0144f2c22d1b634c89cc288765697dc4f3874
Archangeloo <gustavolucasarchangelo07@gmail.com>
2024-09-11 17:30:45 -0300

    cadastrar meta

--- index.js
+++ index.js
@@ -1,14 +1,59 @@
+const { select, input } = require('@inquirer/prompts')
+
 let meta = {
-    value: 'Ler um livro por mês',
-    checked: true,
+    value: "Tomar 3L de água por dia",
+    checked: false,
 }
 
-let metas = [
-    meta,
-    {
-        value:'Caminhar por 20 min todos os dias',
-        checked: false
-    }
-]
+let metas = []
 
-console.log(metas[1].value)
+const cadastrarMeta = async () => {
+    const meta = await input ({message: "Digite a meta:"})
+
+    if(meta.length == 0){
+        console.log("A meta não pode ser vazia!")
+        return
+    }
+
+    metas.push({value: meta, checked: false})
+
+}
+
+const start = async () => {
+
+    
+    while(true){
+
+        const opcao = await select({
+            message: "Menu >",
+            choices: [
+                {
+                    name: "Cadastrar meta",
+                    value: "cadastrar"
+                },
+                {
+                    name: "Listar metas",
+                    value: "listar"
+                },
+                {
+                    name: "Sair",
+                    value: "sair"
+                }
+            ]
+        })
+
+        switch(opcao) {
+            case "cadastrar":
+                await cadastrarMeta()
+                console.log(metas)
+                break
+            case "listar":
+                console.log("vamos listar")
+                break
+            case "sair":
+                console.log("Até a próxima!")
+                return
+        }
+    }
+}
+start()
