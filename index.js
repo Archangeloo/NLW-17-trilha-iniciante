107f05890f00890390fc821494b5d9accea3a1fd
Archangeloo <gustavolucasarchangelo07@gmail.com>
2024-09-11 17:07:24 -0300

    adiciona inquirer

---
+++ index.js
@@ -0,0 +1,14 @@
+let meta = {
+    value: 'Ler um livro por mês',
+    checked: true,
+}
+
+let metas = [
+    meta,
+    {
+        value:'Caminhar por 20 min todos os dias',
+        checked: false
+    }
+]
+
+console.log(metas[1].value)
