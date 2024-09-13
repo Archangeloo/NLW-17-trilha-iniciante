758593a28bf7d3bc8caebef44fbd62d7642f0d02
Archangeloo <gustavolucasarchangelo07@gmail.com>
2024-09-13 17:37:51 -0300

    persistir dados

--- index.js
+++ index.js
@@ -1,23 +1,38 @@
 const { select, input, checkbox } = require('@inquirer/prompts')
+const fs = require("fs").promises
 
-let meta = {
-    value: 'Tomar 3L de água por dia',
-    checked: false,
+
+let mensagem = "Bem vindo ao app de metas"
+
+
+let metas
+
+const carregarMetas = async () => {
+    try {
+        const dados = await fs.readFile("metas.json", "utf-8")
+        metas = JSON.parse(dados)
+    }
+    catch (erro) {
+        metas = []
+    }
 }
 
-let metas = [ meta ]
+const salvarMetas = async () => {
+    await fs.writeFile("metas.json", JSON.stringify(metas, null, 2))
+}
 
 const cadastrarMeta = async () => {
-    const meta = await input({ message: "Digite a meta:"})
+    const meta = await input({ message: "Digite a meta:" })
 
-    if(meta.length == 0) {
-        console.log('A meta não pode ser vazia.')
+    if (meta.length == 0) {
+        mensagemm = 'A meta não pode ser vazia.'
         return
     }
 
     metas.push(
         { value: meta, checked: false }
     )
+    mensagem = "Meta cadastrada com sucesso! :)"
 }
 
 const listarMetas = async () => {
@@ -27,14 +42,16 @@ const listarMetas = async () => {
         instructions: false,
     })
 
-    if(respostas.length == 0) {
-        console.log("Nenhuma meta selecionada!")
-        return
-    }
-
     metas.forEach((m) => {
         m.checked = false
     })
+
+    if (respostas.length == 0) {
+        mensagem = "Nenhuma meta selecionada!"
+        return
+    }
+
+
 
     respostas.forEach((resposta) => {
         const meta = metas.find((m) => {
@@ -44,7 +61,7 @@ const listarMetas = async () => {
         meta.checked = true
     })
 
-    console.log('Meta(s) marcadas como concluída(s)')
+    mensagem = 'Meta(s) marcadas como concluída(s)'
 
 }
 
@@ -52,22 +69,50 @@ const metasRealizadas = async () => {
     const realizadas = metas.filter((meta) => {
         return meta.checked
     })
-    
-    if(realizadas.length == 0) {
-        console.log("Não existem metas realizadas! :(  ")
+
+    if (realizadas.length == 0) {
+        mensagem = "Não existem metas realizadas! :(  "
         return
     }
 
     await select({
-        message: "Metas realizadas",
+        message: "Metas realizadas: " + realizadas.length,
         choices: [...realizadas]
     })
 }
 
+const metasAbertas = async () => {
+    const abertas = metas.filter((meta) => {
+        return meta.checked != true
+    })
+
+    if (abertas.length == 0) {
+        mensagem = "Não existem metas abertas! :)"
+        return
+    }
+    await select({
+        message: "Metas abertas: " + abertas.length,
+        choices: [...abertas]
+    })
+}
+
+const mostrarMensagem = () => {
+    console.clear()
+
+    if (mensagem != "") {
+        console.log(mensagem)
+        console.log("")
+        mensagem = ""
+    }
+}
 
 const start = async () => {
-    while(true){
-        
+    carregarMetas()
+
+    while (true) {
+
+        await mostrarMensagem()
+        await salvarMetas()
         const opcao = await select({
             message: "Menu >",
             choices: [
@@ -80,6 +125,10 @@ const start = async () => {
                     value: "listar"
                 },
                 {
+                    name: "Metas abertas",
+                    value: "abertas"
+                },
+                {
                     name: "Metas Realizadas",
                     value: "realizadas"
                 },
@@ -90,16 +139,18 @@ const start = async () => {
             ]
         })
 
-        switch(opcao) {
+        switch (opcao) {
             case "cadastrar":
                 await cadastrarMeta()
-                console.log(metas)
                 break
             case "listar":
                 await listarMetas()
                 break
             case "realizadas":
                 await metasRealizadas()
+                break
+            case "abertas":
+                await metasAbertas()
                 break
             case "sair":
                 console.log('Até a próxima!')
